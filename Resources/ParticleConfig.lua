2d0cda52b871ebcb51663152fd485e6cd6a0896e
sss <SlawekStrug98@gmail.com>
2023-03-21 19:16:22 +0100

    Settings update

--- Resources/ParticleConfig.lua
+++ Resources/ParticleConfig.lua
@@ -1,11 +1,18 @@
--- Simulation settings
+-- Simulation settings basic
 ParticleSpeed = 0.67
 Alpha = 180.0
 Beta = 17.0
 ReactRadius = 5.0
+
+-- Interesting settings
+--ParticleSpeed = 0.92
+--Alpha = 90
+--Beta = 3
+--ReactRadius = 15.0
+
 -- Simulation bound
-BoundX = 200.0
-BoundY = 200.0
+BoundX = 100.0
+BoundY = 100.0
 -- Other
 ThreadNumber = 10
-ThreadPerBlock = 128
+ThreadPerBlock = 512
